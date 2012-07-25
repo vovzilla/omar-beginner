f9f49bf566b6133f28bf7e76ca5c2177314ced5f
Omar <vovzilla@gmail.com>
2012-07-25 13:03:11 -0500

    -noise v2

--- player.rb
+++ player.rb
@@ -1,94 +1,93 @@
 class Player
-  def initialize
-    @health = 20
-    @drctn = :forward 
-  end
-##################
-  def play_turn(warrior)
-  p @drctn
-   chckhlth(warrior)
+    def initialize
+        @health = 20
+        @drctn = :forward
+    end
+    ##################
+    def play_turn(warrior)
+        p @drctn
+        chckhlth(warrior)
+        if !cptv(warrior)
+            if !bttl(warrior)
+                nxt(warrior)
+            end
 
-      if !cptv(warrior)
-        if !bttl(warrior)
-            nxt(warrior)
         end
+    end
+    ####################
 
+    #####################
+    def cptv(warrior)
+        if
+            warrior.feel(:forward).captive?
+            warrior.rescue!(:forward)
+            else
+            false
+        end
     end
-  end
-####################
-
-#####################
-  def cptv(warrior)
-    if 
-      warrior.feel(:forward).captive?
-      warrior.rescue!(:forward)
-    else
-      false
+    ####################
+    def nxt(warrior)
+        if
+            @dmg
+            if
+                @health < 12
+                rtrt(warrior)
+                else
+                warrior.walk!(:forward)
+            end
+            else
+            if
+                @health < 20
+                warrior.rest!
+                else
+                warrior.walk!(:forward)
+            end
+        end
     end
-  end
-####################
-  def nxt(warrior)
-    if 
-      @dmg
-      if 
-        @health < 12
-        rtrt(warrior)
-      else
-        warrior.walk!(:forward)
-      end
-    else
-      if 
-        @health < 20
-        warrior.rest!
-      else
-        warrior.walk!(:forward)
-      end
+    ####################
+    def chckhlth(warrior)
+        hlth = warrior.health
+        if
+            hlth == 20
+            @health = 20
+        end
+        if
+            hlth < @health
+            @dmg = true
+            else
+            @dmg = false
+        end
+        @health = hlth
     end
-  end
-####################
-  def chckhlth(warrior)
-    hlth = warrior.health
-    if 
-      hlth == 20
-      @health = 20
+    ####################
+    def rtrt(warrior)
+        if :forward == :forward
+            warrior.walk!(:backward)
+            else
+            warrior.walk!(:forward)
+        end
     end
-    if 
-      hlth < @health
-      @dmg = true
-    else
-      @dmg = false
+    ####################
+    def bttl(warrior)
+        if
+            warrior.feel(:forward).enemy?
+            else
+            warrior.shoot!(:forward)
+        end
     end
-    @health = hlth
-  end
-####################
-  def rtrt(warrior)
-    if :forward == :forward
-      warrior.walk!(:backward)
-    else
-      warrior.walk!(:forward)
+    ####################
+    def lok(warrior)
+        if
+            warrior.look
+            #variables
+        end
     end
-  end
-####################
-  def bttl(warrior)
-      if 
-        warrior.feel(:forward).enemy?
-      else
-        warrior.shoot!(:forward)
-      end
-  end
-####################
-  def lok(warrior)
-   if 
-    warrior.look
-   #variables
-   end 
-  end
-###################
-  def sht(warrior)
-   if 
-    warrior.shoot!(:forward)
-    #variables
-   end
-  end
-##################
+    ###################
+    def sht(warrior)
+        if
+            warrior.shoot!(:forward)
+            #variables
+        end
+    end
+    ##################
 end
